d78351c10bcbba139127e712eb08c34acadc735c
Ronen Shekel <shachar.resisi@mail.huji.ac.il>
2022-09-21 15:03:14 +0300

    small

--- show_scan_result.py
+++ show_scan_result.py
@@ -12,5 +12,7 @@ name = os.path.basename(path)
 
 r = ScanResult()
 r.loadfrom(path)
+print(f'integration_time: {r.integration_time}')
 r.show(show_singles=True, remove_accidentals=True,  title=name)
+r.show(show_singles=False, remove_accidentals=False,  title=name)
 plt.show()
